f1aeb1aece676d81b31d24a69dc1f75eb3c4a44f
Jefferson Clementino Ferreira <43593688+stringercj12@users.noreply.github.com>
2021-12-01 00:03:32 -0300

    Modal & Forms - Parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 import "./App.css";
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalSyle } from './styles/global';
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModalOpen() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModalOpen} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModalOpen}
+      />
+
       <GlobalSyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -31,6 +31,7 @@ export const GlobalSyle = createGlobalStyle`
       font-size: 87.5%;
     }
   }
+
   body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
     font-weight: 400;
@@ -53,4 +54,42 @@ export const GlobalSyle = createGlobalStyle`
    opacity: 0.6;
    cursor: not-allowed;
   }
+
+  .react-modal-overlay{
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding:  3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
+  .react-modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover{
+      filter: brightness(0.8);
+    }
+  }
+
 `;
